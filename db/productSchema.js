3a1f26b2c423d142a69a24ba151f505bb617ebdd
Wolf Ferenc <fwolf110@gmail.com>
2023-04-02 18:37:21 +0200

    products model update

--- db/productSchema.js
+++ db/productSchema.js
@@ -5,6 +5,20 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
+  price: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+
+});
+
+productSchema.post('save', function(doc, next) {
+  console.log('A new product was saved to the database:', doc);
+  next();
 });
 
 const Product = model('product', productSchema);
